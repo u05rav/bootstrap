03419db434288a74839d2f7aa55592ca902e89fd
Richard Veitch <richardveitch@fb.com>
2021-08-01 20:28:36 -0700

    basic install commands

--- arch/install.sh
+++ arch/install.sh
@@ -1,3 +1,56 @@
 #! /bin/bash
 
+
+if [ $# -ne 1 ]
+then
+    echo usage $0 hostname
+    exit 1
+fi
+
+HOSTNAME=$1
+
 echo "arch install script"
+
+# create a linux partition taking the entore disk
+echo 'type=83' | sudo sfdisk /dev/sda
+
+# format the partition as ext4
+mkfs.ext4 /dev
+
+# mount the new partition
+mount /dev/root_partition /mnt
+
+# install basic packages
+pacstrap /mnt base linux linux-firmware grub vim
+
+# generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# chroot to new system
+arch-chroot /mnt
+
+# set timezone to Pacific
+ln -sf /usr/share/zoneinfo/US/Pacific /etc/localtime
+
+# generate /etc/adjtime
+hwclock --systohc
+
+# Localization
+sed -i "s/#en_US.UTF-8/en_US.UTF-8/" -i /etc/locale.gen
+
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+# setup networking
+echo $HOSTNAME > /etc/hostname
+echo 127.0.0.1  localhost > /etc/hosts
+echo ::.1  localhost >> /etc/hosts
+echo 127.0.1.1 $HOSTNAME > /etc/hosts
+
+# set password
+passwd
+
+# setup bootloader
+grub-install --target=i386-pc /dev/sda
+
+
+
